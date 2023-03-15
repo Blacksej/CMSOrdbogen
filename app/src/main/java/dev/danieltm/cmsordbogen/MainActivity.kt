7e4c0337f6fba44d6f5005ec68ee0fd04c3e1a34
Daniel Madsen <steamblacksej2@gmail.com>
2023-03-15 14:36:12 +0100

    Added bottom navigation bar with a home, create and posts button. Also added some of Ordbogens colors.

--- app/src/main/java/dev/danieltm/cmsordbogen/MainActivity.kt
+++ app/src/main/java/dev/danieltm/cmsordbogen/MainActivity.kt
@@ -3,26 +3,63 @@ package dev.danieltm.cmsordbogen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import dev.danieltm.cmsordbogen.Models.BottomNavItem
+import dev.danieltm.cmsordbogen.ViewModels.MainViewModel
 import dev.danieltm.cmsordbogen.ui.theme.CMSOrdbogenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val mainViewModel = viewModel<MainViewModel>()
+
             CMSOrdbogenTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = mainViewModel.getNavList(),
+                            navController = navController,
+                            modifier = Modifier
+                                .clip(
+                                    RoundedCornerShape(
+                                        topStart = 15.dp,
+                                        topEnd = 15.dp
+                                    )
+                                )
+                                .height(70.dp),
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                ){
+                        paddingValues -> Modifier.padding(paddingValues)
+                    Navigation(navController = navController)
                 }
             }
         }
@@ -30,14 +67,123 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation(navController: NavHostController)
+{
+    NavHost(navController = navController, startDestination = "home"){
+        composable("home"){
+            // REPRESENTS HOME SCREEN
+            HomeScreen()
+        }
+        composable("create"){
+            // REPRESENTS CREATE SCREEN
+            CreateScreen()
+        }
+        composable("posts"){
+            // REPRESENTS POSTS SCREEN
+            PostsScreen()
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    CMSOrdbogenTheme {
-        Greeting("Android")
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = colorResource(id = R.color.bottom_bar_bg),
+        elevation = 5.dp
+
+    ) {
+        items.forEach{ item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = colorResource(id = R.color.bottom_bar_selected),
+                unselectedContentColor = colorResource(id = R.color.bottom_bar_unselected),
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        if(item.badgeCount > 0){
+                            BadgedBox(
+                                badge =
+                                {
+                                    Badge{Text(item.badgeCount.toString())}
+                                }
+                            ){
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name,
+                                    modifier = Modifier
+                                        .size(30.dp)
+                                )
+                            }
+                        } else{
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name,
+                                modifier = Modifier
+                                    .size(30.dp)
+                            )
+                        }
+                        if(selected && item.route != "create"){
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
+@Composable
+fun HomeScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background_home)),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Home Screen",
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun CreateScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background_create)),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Create Screen",
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun PostsScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background_posts)),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Posts Screen",
+            color = Color.Black
+        )
     }
 }
---
+++ app/src/main/java/dev/danieltm/cmsordbogen/ViewModels/MainViewModel.kt
@@ -0,0 +1,33 @@
+package dev.danieltm.cmsordbogen.ViewModels
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.lifecycle.ViewModel
+import dev.danieltm.cmsordbogen.Models.BottomNavItem
+
+class MainViewModel : ViewModel(){
+
+    fun getNavList() : List<BottomNavItem>
+    {
+        var navItems = listOf(
+            BottomNavItem(
+                name = "HJEM",
+                route = "home",
+                icon = Icons.Default.Home
+            ),
+            BottomNavItem(
+                name = "",
+                route = "create",
+                icon = Icons.Default.AddCircle
+            ),
+            BottomNavItem(
+                name = "INDLÆG",
+                route = "posts",
+                icon = Icons.Default.Menu
+            ),
+        )
+        return navItems
+    }
+}
